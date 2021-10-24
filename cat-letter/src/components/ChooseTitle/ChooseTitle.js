e83afa35dca0e603edc8ad8d60e99a65f77b5f96
kjs <k021229@naver.com>
2021-10-24 21:28:27 +0900

    타이틀 이미지 변경 로직 추가

--- cat-letter/src/components/ChooseTitle/ChooseTitle.js
+++ cat-letter/src/components/ChooseTitle/ChooseTitle.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import SelectCat from '../../static/Main/cat.png';
 import SelectGhost from '../../static/Main/ghost.png';
@@ -27,28 +27,46 @@ const ResultImgContainer = styled.div`
 `;
 
 const ChooseTitle = () => {
-  return (
-    <>
-      <ChooseImgWrppaer>
-          <ChooseImgContainer>
-            <TitleImg src={SelectCat} />
-          </ChooseImgContainer>
-          <ChooseImgContainer>
-            <TitleImg src={SelectGhost} />
-          </ChooseImgContainer>
-          <ChooseImgContainer>
-            <TitleImg src={SelectPumpkin} />
-          </ChooseImgContainer>
-          <ChooseImgContainer>
-            <TitleImg src={SelectSkull} />
-          </ChooseImgContainer>
-        </ChooseImgWrppaer>
+    const [pictureState, setPictureState] = useState(0);
+    const onChange = (number) => {
+        setPictureState(number);
+    }
+    const getImage = (number) => {
+        switch (number) {
+            case 0:
+                return SelectCat
+            case 1:
+                return SelectGhost
+            case 2:
+                return SelectPumpkin
+            case 3:
+                return SelectSkull
+            default:
+                return SelectCat
+        }
+    }
+    return (
+        <>
+            <ChooseImgWrppaer>
+                <ChooseImgContainer>
+                    <TitleImg src={SelectCat} onClick={() => onChange(0)} />
+                </ChooseImgContainer>
+                <ChooseImgContainer>
+                    <TitleImg src={SelectGhost} onClick={() => onChange(1)} />
+                </ChooseImgContainer>
+                <ChooseImgContainer>
+                    <TitleImg src={SelectPumpkin} onClick={() => onChange(2)} />
+                </ChooseImgContainer>
+                <ChooseImgContainer>
+                    <TitleImg src={SelectSkull} onClick={() => onChange(3)} />
+                </ChooseImgContainer>
+            </ChooseImgWrppaer>
 
-        <ResultImgContainer>
-          <TitleImg src={SelectCat} />
-        </ResultImgContainer>
-    </>
-  );
+            <ResultImgContainer>
+                <TitleImg src={getImage(pictureState)} />
+            </ResultImgContainer>
+        </>
+    );
 };
 
 export default ChooseTitle;
